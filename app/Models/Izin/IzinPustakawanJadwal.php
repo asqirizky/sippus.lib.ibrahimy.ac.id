6c560ec41fcbbc2a732dee56749eb4f440bf2a59
Yogy Asqinal Rizky <asqirizky@gmail.com>
2026-07-28 23:05:55 +0700

    maintenance commit

--- app/Models/Izin/IzinPustakawanJadwal.php
+++ app/Models/Izin/IzinPustakawanJadwal.php
@@ -2,12 +2,18 @@
 
 namespace App\Models\Izin;
 
+use App\Models\Master\Jadwal;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class IzinPustakawanJadwal extends Model
 {
-    //
     protected $table = 'izin_pustakawan_jadwal';
 
     protected $guarded = ['id'];
+
+    public function jadwal(): BelongsTo
+    {
+        return $this->belongsTo(Jadwal::class);
+    }
 }
